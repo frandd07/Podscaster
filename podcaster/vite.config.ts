872ca084eb0a6b3c95d4b1685358aaaf7e4edcc5
DavidGmezHdez <david.gomez@nomasystems.com>
2025-04-03 13:29:50 +0200

    fix: vite error

--- podcaster/vite.config.ts
+++ podcaster/vite.config.ts
@@ -5,13 +5,4 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      provider: 'v8',
-      include: ['**/*.tsx'],
-    },
-    css: true,
-  },
 })
